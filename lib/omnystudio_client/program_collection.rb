5d9a9fb9737b05c263100642e390a4f892114a94
arellaTV <jeremiah.arella@gmail.com>
2020-01-30 15:56:09 -0800

    Removes network from program collection handler

--- lib/omnystudio_client/program_collection.rb
+++ lib/omnystudio_client/program_collection.rb
@@ -9,22 +9,19 @@ module OmnyStudioClient
 
     # @return a OmnyStudioClient
     # @note This is used as a way to access top level attributes
-    # @example Accessing a network id
-    #   config.network_id #=> '{network id specified in initialization}'
 
     def config
       @config ||= OmnyStudioClient
     end
 
     # @return an array of structs that represents a list of programs
-    # @note It needs to be initialized with a network id
     # @see OmnyStudioClient#connection
     # @example Get a list of all programs
     #   omnystudio.programs.list #=> An array of structs representing a list of programs
 
     def list options={}
       OmnyStudioClient.connection({
-        :url => "#{config.api_base_url}/networks/#{config.network_id}/programs",
+        :url => "#{config.api_base_url}/programs",
         :method => :get
       })
     end
